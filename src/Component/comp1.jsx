6bf4f0de435bc5ebafa00b81feb70a5f6eaf28f1
Sachin Raj <sachinraj6392@gmail.com>
2026-07-18 15:55:36 +0530

    implementing testing

--- src/Component/comp1.jsx
+++ src/Component/comp1.jsx
@@ -47,7 +47,7 @@ function CITest() {
     }
 
     return (
-        <div className="min-h-screen bg-gray-100 py-10">
+        <div className="min-h-screen bg-gray-100 py-11">
             <div className="mx-auto max-w-4xl px-4">
                 <h1 className="mb-8 text-center text-4xl font-bold text-gray-800">
                     React CI Demo
--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 export default defineConfig({
   plugins: [react()],
-
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "./src/setupTests.js",
-  },
-});
+})
